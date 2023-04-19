518e03ba5fced26c4c04394829adf86d3d6e0c23
agussiro06 <102998952+agussiro06@users.noreply.github.com>
2023-04-19 09:08:48 -0300

    Add files via upload

--- js/code.js
+++ js/code.js
@@ -1,7 +1,18 @@
 function ConseguirProducto(){
+    console.log("Hago consulta - fetch")
     var producto=document.getElementById("producto").value
-fetch(`https://dummyjson.com/products/search?q=${producto}`)
-.then(res => res.json())
-.then(json => console.log(json))
-.then(json=> console.log(json.title))
+	fetch(`https://dummyjson.com/products/search?q=${producto}`)
+        .then(res => res.json())
+        .then(res => {
+            console.log("obtuve respuesta",res.products)
+            document.getElementById("valores").innerHTML=""
+            valores = document.getElementById("valores");
+            res.products.forEach(producto => {
+                item=document.createElement("li")
+                item.innerHTML=`Nombre: ${producto.title}`
+                valores.appendChild(item)
+            })
+        })
+        .catch(err => console.error("error", err))
+    console.log("Fin consulta - fetch")
 }
