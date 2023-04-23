bc49187f6e3892b3d1008b40bedcbf2fba094201
Agustin Sirotinsky <agussiro06@gmail.com>
2023-04-23 15:11:28 -0300

    algo

--- js/code.js
+++ js/code.js
@@ -1,18 +1,25 @@
 function ConseguirProducto(){
     console.log("Hago consulta - fetch")
-    var producto=document.getElementById("producto").value
-	fetch(`https://dummyjson.com/products/search?q=${producto}`)
+    var title=document.getElementById("title").value
+    // var category=document.getElementById("category").value
+	fetch(`https://dummyjson.com/products/search?q=${title}`)
         .then(res => res.json())
         .then(res => {
             console.log("obtuve respuesta",res.products)
             document.getElementById("valores").innerHTML=""
             valores = document.getElementById("valores");
-            res.products.forEach(producto => {
+            res.products.forEach(title => {
                 item=document.createElement("li")
-                item.innerHTML=`Nombre: ${producto.title}`
+                item.innerHTML=`Nombre: ${title.title}`
                 valores.appendChild(item)
             })
         })
         .catch(err => console.error("error", err))
     console.log("Fin consulta - fetch")
 }
+(function CargarCategorias(){
+    console.log("Cargo categorias")
+    fetch('https://dummyjson.com/products/categories')
+    .then(res => res.json())
+    .then(console.log);
+})();
