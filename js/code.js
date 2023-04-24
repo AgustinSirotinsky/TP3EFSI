0bf6d3f7a63ae6f24e9ae11b4bafb0b6ffb1e5ad
AGUSTÍN SIROTINSKY <46645963@ort>
2023-04-24 08:18:32 -0300

    lol

--- js/code.js
+++ js/code.js
@@ -9,7 +9,18 @@ function ConseguirProducto(){
             valores = document.getElementById("valores");
             res.products.forEach(product => {
                 item=document.createElement("li")
-                item.innerHTML=`<a href="DetalleProducto.html?id=${product.id}" target="_blank">${product.title}</a>`
+                // item.innerHTML=`<a href="DetalleProducto.html?id=${product.id}" target="_blank">${product.title}</a>`
+                item.innerHTML=
+                `
+                <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="${product.thumbnail}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">${product.title}</h5>
+                    <p class="card-text">${product.description}</p>
+                    <a href="DetalleProducto.html?id=${product.id}" target="_blank" class="btn btn-primary">${product.title}</a>
+                </div>
+                </div>
+                `
                 valores.appendChild(item)
             })
         })
