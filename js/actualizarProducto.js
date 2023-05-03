c0b55f79343e5b57c7d24b19b556788e95adcaa4
AGUSTÍN SIROTINSKY <46645963@ort>
2023-05-03 08:34:07 -0300

    actualizar producto hecho

--- js/actualizarProducto.js
+++ js/actualizarProducto.js
@@ -2,5 +2,20 @@ const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 
 function ActualizarProducto(){
-    
+    console.log("Recibi Actualizar producto")
+    var title=document.getElementById("title").value
+    var price=document.getElementById("price").value
+    var description=document.getElementById("description").value
+    fetch(`https://dummyjson.com/products/${params.id}`, {
+  method: 'PUT', /* or PATCH */
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    title: title,
+    price: price,
+    description: description
+  })
+})
+.then(res => res.json())
+.then(console.log);
+window.location.href = "index.html";
 }
